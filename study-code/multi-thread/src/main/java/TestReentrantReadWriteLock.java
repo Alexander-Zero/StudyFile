8673029d9ec336be935dfa5af27bc54280ce86c6
Alexander-Zero <51397937+Alexander-Zero@users.noreply.github.com>
2020-11-24 23:01:32 +0800

    dss
    v b

---
+++ study-code/multi-thread/src/main/java/TestReentrantReadWriteLock.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @version 1.0.0
+ * @auther Alexander Zero
+ * @date 2020/11/24
+ */
+public class TestReentrantReadWriteLock {
+    static ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    static ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
+    static ReentrantReadWriteLock.WriteLock writeLock = lock.writeLock();
+    static Random random = new Random();
+    private static int value;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 18; i++) {
+            new Thread(() -> read(readLock)).start();
+        }
+        for (int i = 0; i < 2; i++) {
+            new Thread(() -> write(writeLock, random.nextInt(20))).start();
+        }
+    }
+
+    public static void read(Lock lock) {
+        try {
+            Thread.sleep(1000);
+            lock.lock();
+            System.out.println("读取成功:" + value);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void write(Lock lock, int value) {
+        try {
+            Thread.sleep(1000);
+            lock.lock();
+            TestReentrantReadWriteLock.value = value;
+            System.out.println("写入成功:" + value);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
