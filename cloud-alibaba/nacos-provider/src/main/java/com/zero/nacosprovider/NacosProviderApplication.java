3a603fcad8885aeccebcf6de3192f7856989d807
Alexander-Zero <cms19940109@163.com>
2021-04-25 16:41:58 +0800

    nacos-config
    nacos-config

--- cloud-alibaba/nacos-provider/src/main/java/com/zero/nacosprovider/NacosProviderApplication.java
+++ cloud-alibaba/nacos-provider/src/main/java/com/zero/nacosprovider/NacosProviderApplication.java
@@ -3,9 +3,6 @@ package com.zero.nacosprovider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 
 /**
@@ -20,13 +17,4 @@ public class NacosProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosProviderApplication.class, args);
     }
-
-    @RestController
-    public class EchoController {
-        @GetMapping("/echo/{string}")
-        public String echo(@PathVariable String string) {
-            return "Hello Nacos Discovery " + string;
-        }
-    }
-
 }
