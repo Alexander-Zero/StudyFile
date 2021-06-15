40060dc6125c409b7f03124869ccdbcff70a9fdf
Alexander-Zero <cms19940109@163.com>
2021-06-15 17:18:39 +0800

    A

--- online-taxi/cloud-eureka/src/main/java/com/zero/cloudeureka/CloudEurekaApplication.java
+++ online-taxi/cloud-eureka/src/main/java/com/zero/cloudeureka/CloudEurekaApplication.java
@@ -3,8 +3,10 @@ package com.zero.cloudeureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 
 /**
  * @author Zero
@@ -13,12 +15,14 @@ import org.springframework.context.ApplicationListener;
 @EnableEurekaServer
 public class CloudEurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudEurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaApplication.class, args);
+    }
 
-	public void test(){
-		ApplicationEvent
-		ApplicationListener
-	}
+
+    @EventListener
+    public void listen(EurekaInstanceCanceledEvent event) {
+        System.out.println("服务注销");
+    }
+
 }
